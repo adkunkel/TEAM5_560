f5637402f30c1dc353e04220d99c0c80c1a4df1f
Robbieo <Robbieo@E1113-18-1815>
2019-11-24 17:52:14 -0600

    NonReportQuery page created.

---
+++ DataAccess/UserInterface/NonReportQuery.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserInterface
+{
+    /// <summary>
+    /// Interaction logic for NonReportQuery.xaml
+    /// </summary>
+    public partial class NonReportQuery : Page
+    {
+        public NonReportQuery()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// NonReportQuery.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void OneButton(object sender, EventArgs args)
+        {
+            //Query 1
+        }
+    }
+}
