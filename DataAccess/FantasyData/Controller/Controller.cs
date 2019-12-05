ed809f947d86018c9bb9e1c81df08c172cf66e43
Blake Wewer <bewewer@yahoo.com>
2019-12-04 19:46:05 -0600

    PlayerInfo to Array

--- DataAccess/FantasyData/Controller/Controller.cs
+++ DataAccess/FantasyData/Controller/Controller.cs
@@ -7,14 +7,43 @@ namespace FantasyData.Controller
 {
     class Controller
     {
-        public string[,] Players = new string[532,16];
+        public Models.Player.PlayerInfo[] player_infos = new Models.Player.PlayerInfo[532];
         
         public void fillArray(string filepath)
         {
             StreamReader reader = new StreamReader(filepath);
-            while(!reader.EndOfStream)
+            reader.ReadLine();
+            int i = 0;
+            while (!reader.EndOfStream)
             {
-                string 
+                string raw_player_info = reader.ReadLine();
+                string[] raw_player_info_split = raw_player_info.Split('|');
+                Player.QBRWTE.Position position = Player.QBRWTE.Position.QB;
+                switch(raw_player_info_split[5])
+                {
+                    case "QB":
+                        position = Player.QBRWTE.Position.QB;
+                        break;
+                    case "RB":
+                        position = Player.QBRWTE.Position.RB;
+                        break;
+                    case "WR:":
+                        position = Player.QBRWTE.Position.WR;
+                        break;
+                    case "TE":
+                        position = Player.QBRWTE.Position.TE;
+                        break;
+                }
+
+                Player.PlayerInfo player_info = new Player.PlayerInfo();
+                player_info.Name = raw_player_info_split[0];
+                player_info.Height = Convert.ToInt32(raw_player_info_split[1]);
+                player_info.Weight = Convert.ToInt32(raw_player_info_split[2]);
+                player_info.YearsPro = Convert.ToInt32(raw_player_info_split[3]);
+                player_info.BirthDate = Convert.ToDateTime(raw_player_info_split[4]);
+                player_info.Position = position;
+                
+                player_infos[i] = player_info;
             }
         }
 
@@ -24,7 +53,7 @@ namespace FantasyData.Controller
         /// <param name="p"></param>
         public void InsertQB(Player p)
         {
-            sql =
+ //           sql =
         }
 
     }
