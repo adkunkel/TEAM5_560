f496f8d3f66b0e3cdeb685b8083561611d47089a
Robbieo <Robbieo@E1113-14-1811>
2019-12-09 20:59:33 -0600

    Commits by rob

--- DataAccess/UserInterface/ReportQuery.xaml.cs
+++ DataAccess/UserInterface/ReportQuery.xaml.cs
@@ -86,8 +86,10 @@ namespace UserInterface
                 //CHANGE THIS TO THE REPORT QUERY
                 sql = $"select t.TeamName, tp.Name, pi.Position, sum(ps.PassYards) as PassYards, sum(ps.RushYards) as RushYards, sum(ps.ReceivingYards) as ReceivingYards, " +
                     $"sum(ps.Receptions) as Receptions, sum(ps.Touchdowns) as Touchdowns, sum(ps.Interceptions) as Interceptions, sum(ps.Fumbles) as Fumbles " +
-                    $"from NFL.Teams t inner join Players.TeamPlayer tp on tp.TeamID = t.TeamID inner join Players.PlayerInfo pi on pi.PlayerID = tp.PlayerID " +
-                    $"inner join Players.PlayerStats ps on ps.PlayerID = tp.PlayerID " +
+                    $"from NFL.Teams t " +
+                    $"inner join Players.TeamPlayer tp on tp.TeamID = t.TeamID " +
+                    $"inner join Players.PlayerInfo pi on pi.PlayerID = tp.PlayerID " +
+                    $"left join Players.PlayerStats ps on ps.PlayerID = tp.PlayerID " +
                     $"where t.TeamName = '{team}' group by t.TeamName, tp.Name, pi.Position";
                 command = new SqlCommand(sql, connection);
                 dataReader = command.ExecuteReader();
